5164a5ea02407e587af07c23ff0f8e3d0fc6009b
Habeeb Mac-iver <habeebmaciver@gmail.com>
2025-05-13 17:05:14 +0100

    Setup swagger documentation lib

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('PAAN API Server')
+    .setDescription('Documentation for the PAAN API Server')
+    .setVersion('1.0')
+    .addTag('paan')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 4200);
 }
+
 bootstrap();
