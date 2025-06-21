68b4782730742b8128d99d856c86ffb26b0f7f03
Habeeb Mac-iver <habeebmaciver@gmail.com>
2025-06-21 10:56:43 +0100

    Allow CORS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://paancircle.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // if using cookies or auth headers
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
