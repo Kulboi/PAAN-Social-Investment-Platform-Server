96cc09ff7670ae84546039dd21669c3b891e5a16
Habeeb Mac-iver <habeebmaciver@gmail.com>
2025-06-21 11:15:50 +0100

    Debugging CORS error

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://paancircle.app'],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // if using cookies or auth headers
+    // credentials: true,
   });
 
   app.useGlobalPipes(
