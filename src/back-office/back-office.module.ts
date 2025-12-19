56e3294d00945d54510520f92ca20e6e9434832e
Habeeb Mac-iver <habeebmaciver@gmail.com>
2025-12-19 17:39:53 +0100

    feat: add CompaniesService to BackOfficeModule providers

--- src/back-office/back-office.module.ts
+++ src/back-office/back-office.module.ts
@@ -5,6 +5,7 @@ import { JwtService } from '@nestjs/jwt';
 import { BackOfficeController } from './back-office.controller';
 import { BackOfficeService } from './back-office.service';
 import { InvestmentCategoriesService } from 'src/investment-categories/investment-categories.service';
+import { CompaniesService } from 'src/companies/companies.service';
 
 import { BackOfficeUser } from 'src/back-office/entities/back-office-user.entity';
 import { InvestmentCategory } from 'src/investment-categories/entities/investment-category.entity';
@@ -12,7 +13,7 @@ import { Company } from 'src/companies/entities/company.entity';
 
 @Module({
   controllers: [BackOfficeController],
-  providers: [BackOfficeService, JwtService, InvestmentCategoriesService],
+  providers: [BackOfficeService, JwtService, InvestmentCategoriesService, CompaniesService],
   imports: [TypeOrmModule.forFeature([BackOfficeUser, InvestmentCategory, Company])],
 })
 export class BackOfficeModule {}
