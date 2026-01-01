3318b47e077fce131c3505c2814eecdc5f3689aa
Habeeb Mac-iver <habeebmaciver@gmail.com>
2026-01-01 09:16:32 +0100

    feat: add ApiProperty decorators to create and login back office user DTOs for Swagger documentation

--- src/back-office/dto/create-back-office-user-request.dto.ts
+++ src/back-office/dto/create-back-office-user-request.dto.ts
@@ -5,36 +5,50 @@ import {
   IsString,
   IsEnum,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 import BackOfficeUserRoleTypes from 'src/back-office/enums/back-office-user-role-types.enum';
 import { GenderTypes } from 'src/user/user.enums';
 
 export class CreateBackOfficeUserRequestDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   first_name: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   last_name: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   email: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   username?: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @MinLength(6)
   @IsString()
   password: string;
 
+  @ApiProperty({
+    enum: BackOfficeUserRoleTypes,
+    default: BackOfficeUserRoleTypes.ADMIN,
+    required: false,
+    type: String,
+    description: 'Role of the user',
+  })
   @IsOptional()
   @IsEnum(BackOfficeUserRoleTypes)
   role?: BackOfficeUserRoleTypes;
 
+  @ApiProperty()
   @IsOptional()
   @IsEnum(GenderTypes)
   gender?: GenderTypes;
--- src/back-office/dto/login-back-office-user-request.dto.ts
+++ src/back-office/dto/login-back-office-user-request.dto.ts
@@ -1,10 +1,13 @@
 import { IsNotEmpty, MinLength, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginBackOfficeUserRequestDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   email: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @MinLength(6)
   @IsString()
