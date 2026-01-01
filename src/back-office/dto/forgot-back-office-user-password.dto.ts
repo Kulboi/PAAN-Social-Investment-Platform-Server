4f48aa805e45963b55fdcff94b37e71405d29538
Habeeb Mac-iver <habeebmaciver@gmail.com>
2026-01-01 09:13:14 +0100

    feat: add ApiProperty decorators to password reset DTOs for Swagger documentation

--- src/back-office/dto/forgot-back-office-user-password.dto.ts
+++ src/back-office/dto/forgot-back-office-user-password.dto.ts
@@ -1,12 +1,15 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ForgotBackOfficeUserPasswordDto {
+  @ApiProperty()
   @IsEmail()
   @IsNotEmpty()
   email: string;
 }
 
 export class ForgotBackOfficeUserPasswordResponseDto {
+  @ApiProperty()
   @IsString()
   message: string;
 }
--- src/back-office/dto/reset-back-office-user-password.dto.ts
+++ src/back-office/dto/reset-back-office-user-password.dto.ts
@@ -1,10 +1,13 @@
 import { IsString, IsNotEmpty, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ResetBackOfficeUserPasswordRequestDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   token: string;
 
+  @ApiProperty()
   @MinLength(6)
   @IsString()
   @IsNotEmpty()
@@ -12,6 +15,7 @@ export class ResetBackOfficeUserPasswordRequestDto {
 }
 
 export class ResetBackOfficeUserPasswordResponseDto {
+  @ApiProperty()
   @IsString()
   message: string;
 }
