bd936a6e87eb8f57ab592b6a0a9119181e1c961a
Habeeb Mac-iver <habeebmaciver@gmail.com>
2026-01-01 08:08:29 +0100

    feat: implement seeder for super admin user with hardcoded values

--- src/seeders/back-office-users.seeder.ts
+++ src/seeders/back-office-users.seeder.ts
@@ -16,22 +16,32 @@ export class BackOfficeUsersSeeder implements Seeder {
   ) {}
 
   async seed(): Promise<any> {
-    const users = Array.from({ length: 2 }).map(() => {
-      return this.userRepository.create({
-        first_name: faker.person.firstName(),
-        last_name: faker.person.lastName(),
-        email: faker.internet.email(),
-        username: faker.internet.userName(),
-        password: faker.internet.password({ length: 10 }),
-        role: BackOfficeUserRoleTypes.SUPER_ADMIN,
-        is_active: true,
-      });
+    // const users = Array.from({ length: 2 }).map(() => {
+    //   return this.userRepository.create({
+    //     first_name: faker.person.firstName(),
+    //     last_name: faker.person.lastName(),
+    //     email: faker.internet.email(),
+    //     username: faker.internet.userName(),
+    //     password: faker.internet.password({ length: 10 }),
+    //     role: BackOfficeUserRoleTypes.SUPER_ADMIN,
+    //     is_active: true,
+    //   });
+    // });
+
+    const superAdminUser = this.userRepository.create({
+      first_name: 'Super',
+      last_name: 'Admin',
+      email: 'super_admin@paancircle.app',
+      username: 'superadmin',
+      password: 'Paancircle@256!',
+      role: BackOfficeUserRoleTypes.SUPER_ADMIN,
+      is_active: true,
     });
 
-    return this.userRepository.save(users);
+    return this.userRepository.save(superAdminUser);
   }
 
   async drop(): Promise<any> {
-    return this.userRepository.delete({});
+    return this.userRepository.createQueryBuilder('backOfficeUser').delete().execute();
   }
 }
