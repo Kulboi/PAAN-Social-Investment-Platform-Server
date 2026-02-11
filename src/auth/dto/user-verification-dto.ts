f251990604e64c030c934ce6a66eb24ff00bac47
Habeeb Mac-iver <habeebmaciver@gmail.com>
2026-02-11 05:46:21 +0100

    feat: add ApiProperty decorators to ResendOTPDto and UserVerificationDto for improved Swagger documentation

--- src/auth/dto/user-verification-dto.ts
+++ src/auth/dto/user-verification-dto.ts
@@ -1,10 +1,15 @@
 import { IsNotEmpty, IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ResendOTPDto {
+  @ApiProperty({ example: 'user@example.com' })
   @IsEmail() email: string;
 }
 
 export class UserVerificationDto {
+  @ApiProperty({ example: 'user@example.com' })
   @IsEmail() email: string;
+  
+  @ApiProperty({ example: '123456' })
   @IsNotEmpty() otp: string;
 }
